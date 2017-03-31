e345ebb5bff3c4b2b87a32ad251de3e99d2cab52
mcgagnier <mcgagnier@yahoo.com>
2017-03-30 18:53:40 -0600

    Front end basics done

--- services/mainSrvc.js
+++ services/mainSrvc.js
@@ -95,5 +95,60 @@ angular.module('itri').service('mainSrvc', function(){
         distance: "625",
         time: 38,
         desc: "Saturday afternoon in the pool"
-    }]
+    },
+
+    {
+        username: 'mcgagnier',
+        code: 'Run',
+        date: "3/26/17",
+        distance: "6.1",
+        time: 66,
+        desc: "Great training run"
+
+    },{
+        username: 'mcgagnier',
+        code: 'Bike',
+        date: "3/25/17",
+        distance: "11.38",
+        time: 44,
+        desc: "Great training ride"
+
+
+    },{
+        username: 'mcgagnier',
+        code: 'Run',
+        date: "3/23/17",
+        distance: "3",
+        time: 27,
+        desc: "Nice short run"
+
+
+    },{
+        username: 'mcgagnier',
+        code: 'Bike',
+        date: "3/22/17",
+        distance: "8.4",
+        time: 31,
+        desc: "Commute home from work"
+
+
+    },{
+        username: 'mcgagnier',
+        code: 'Run',
+        date: "3/20/17",
+        distance: "4.3",
+        time: 37.5,
+        desc: "Morning run"
+
+
+    },{
+        username: 'mcgagnier',
+        code: 'Swim',
+        date: "3/25/17",
+        distance: "625",
+        time: 38,
+        desc: "Saturday afternoon in the pool"
+    }
+
+]
 })
